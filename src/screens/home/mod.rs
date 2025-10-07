33afc7c80590bb0c3071f26aee9c01ee03c4d8c6
hmziqrs <hmziqrs@gmail.com>
2025-10-07 21:38:10 +0500

    Enhance HomeScreen layout with Tailwind CSS classes for improved styling and responsiveness

--- src/screens/home/mod.rs
+++ src/screens/home/mod.rs
@@ -3,21 +3,21 @@ use dioxus::prelude::*;
 #[component]
 pub fn HomeScreen() -> Element {
     rsx! {
-        div {
-            header {
-                h1 { "Freeoxide"}
-                h3 { "Freedom and open source Rust based software tools" }
+        div { class: "w-full max-w-4xl mx-auto flex flex-col min-h-screen",
+            header { class: "text-center py-8",
+                h1 { class: "text-4xl font-bold mb-4", "Freeoxide"}
+                h3 { class: "text-xl", "Freedom and open source Rust based software tools" }
             }
-            div {
-                p {
+            div { class: "flex-1 flex items-center justify-center px-4",
+                p { class: "text-lg text-center",
                     "Currently working on DIY home security system."
                 }
             }
-            footer {
+            footer { class: "text-center py-4",
                 p { "© 2024 Freeoxide" }
                 p {
                     span { "Built by " }
-                    a {
+                    a { class: "underline",
                         href: "https://hmziq.rs",
                         target: "_blank",
                         "Hmziqrs"
