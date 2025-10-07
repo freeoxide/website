b0aab76e61285d41b8f14f7dd060b7e44ee18cd2
hmziqrs <hmziqrs@gmail.com>
2025-10-07 21:42:02 +0500

    Add askama_escape dependency and enhance meta tags in App component

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,23 @@ const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 #[component]
 fn App() -> Element {
+    let title = "Freeoxide - Freedom and open source Rust based software tools";
+    let description = "Freeoxide provides freedom and open source software tools built with Rust.";
+    let domain = "https://freeoxide.com";
+
     rsx! {
+        document::Title { "{title}" }
+        document::Meta { name: "description", content: "{description}" }
+        document::Meta { name: "keywords", content: "rust, open source, software tools, diy, home security" }
+        document::Meta { name: "author", content: "hmziqrs" }
+        document::Meta { property: "og:title", content: "{title}" }
+        document::Meta { property: "og:description", content: "{description}" }
+        document::Meta { property: "og:url", content: "{domain}" }
+        document::Meta { property: "og:type", content: "website" }
+        document::Meta { name: "twitter:card", content: "summary" }
+        document::Meta { name: "twitter:title", content: "{title}" }
+        document::Meta { name: "twitter:description", content: "{description}" }
+
         document::Link {
             rel: "preconnect",
             href: "https://fonts.googleapis.com"
